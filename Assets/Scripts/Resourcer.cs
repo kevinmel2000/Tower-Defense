8082f68b0c3814c8464fb0f8b7968fc001bab54e
omrobbie <putu.roby@yahoo.com>
2017-05-03 22:13:39 +0800

    spawn coin resourcer

---
+++ Assets/Scripts/Resourcer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Resourcer : Tower
+{
+    public float coinCooldown;
+    public GameObject itemCoin;
+
+    float indexTimeCooldown;
+
+    void SpawnCoin()
+    {
+        Vector3 newPos = new Vector3(this.transform.position.x + Random.Range(-1f, 1f), this.transform.position.y + Random.Range(-1f, 1f), -1f);
+        GameObject bul = Instantiate(itemCoin, newPos, Quaternion.identity) as GameObject;
+    }
+
+    private void Start()
+    {
+        indexTimeCooldown = coinCooldown;
+    }
+
+    private void Update()
+    {
+        if(indexTimeCooldown <= 0)
+        {
+            SpawnCoin();
+            indexTimeCooldown = coinCooldown;
+        }
+        indexTimeCooldown -= Time.deltaTime;
+    }
+}
