3c142e7559d1912b202d9bd381ea68169044b95f
omrobbie <putu.roby@yahoo.com>
2017-05-03 15:22:22 +0800

    susun luas area permainan

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public bool isTaken;
+    public GameObject tower;
+
+    public delegate void OnClickHandler(GameObject thisTile);
+    public static event OnClickHandler Click;
+
+    private void Update()
+    {
+        if (isTaken && (tower == null)) isTaken = false;
+    }
+
+    public void ChangeStatus()
+    {
+        isTaken = !isTaken;
+    }
+
+    private void OnMouseDown()
+    {
+        Click(this.gameObject);
+    }
+}
