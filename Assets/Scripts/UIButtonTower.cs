0b2446c071438aec80073474a5bfd99ab2e53248
omrobbie <putu.roby@yahoo.com>
2017-05-04 08:10:30 +0800

    script untuk button controller

---
+++ Assets/Scripts/UIButtonTower.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class UIButtonTower : MonoBehaviour
+{
+    public delegate void OnUIButtonClickHandler(int indexTower);
+    public static event OnUIButtonClickHandler Click;
+
+    public void OnTowerAttackClick()
+    {
+        Click(0);
+    }
+
+    public void OnTowerDefenderClick()
+    {
+        Click(1);
+    }
+
+    public void OnTowerResourcerClick()
+    {
+        Click(2);
+    }
+}
